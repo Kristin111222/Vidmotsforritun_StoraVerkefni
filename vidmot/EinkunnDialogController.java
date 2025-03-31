01d55d9d25635627c250dbdd300fbebdb914b369
Kristin111222 <skristin86@gmail.com>
2025-03-31 11:13:29 +0000

    Add files via upload
    Einkunn Dialog, setja inn Label og textField og TextArea, gefa einkunn og review

--- vidmot/EinkunnDialogController.java
+++ vidmot/EinkunnDialogController.java
@@ -1,22 +1,49 @@
 package com.example.vidmot;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
-import javafx.scene.control.DialogPane;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.util.Callback;
 
 import java.io.IOException;
 
+import static javafx.application.Application.launch;
+
 public class EinkunnDialogController extends Dialog<String> {
 
-    private Label fxEinkunn;
+    @FXML
+    private TextField fxEinkunn;
+    @FXML
+    private TextArea fxEinkunnFeedback;
+
+    @FXML
+    public void onEinkunn(ActionEvent event) { // svar og ýta á enter
+    }
+
+/*
+    @FXML
+    public void onGefaEinkunn(ActionEvent event){ // svar og ýta á enter
+        String svarNotanda = fxEinkunn.getText();
+
+        try {
+            String feedback = FeedbackService.provideFeedback2(svarNotanda);
+            fxEinkunnFeedback.setText(feedback); // feedback birtist
+
+        } catch (Exception e) {
+            fxEinkunnFeedback.setText("Villuboð");
+            e.printStackTrace();
+        }
+    }
+
+ */
+
+
 
     public EinkunnDialogController() {
 
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("auka-view.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("einkunn-view.fxml"));
             fxmlLoader.setController(this);
             DialogPane rotin = fxmlLoader.load();
             setDialogPane(rotin);
@@ -24,13 +51,18 @@ public class EinkunnDialogController extends Dialog<String> {
             e.printStackTrace();
         }
         setResultConverter(new Callback<ButtonType, String>() {
-            @Override
+           @Override
             public String call(ButtonType buttonType) {
                 String svarNotanda = fxEinkunn.getText().trim();
-                return svarNotanda.isEmpty() ? null : svarNotanda;
+               return svarNotanda.isEmpty() ? null : svarNotanda;
             }
         });
     }
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
 }
 
 
